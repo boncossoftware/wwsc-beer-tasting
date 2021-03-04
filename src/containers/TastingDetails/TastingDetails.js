81cf80c98d2f41fff3f597a50c27f302a03ec2dc
Israel A. Posner <israelposner@boncos.io>
2021-03-04 09:07:32 -0400

    Added basic structure for tastingdetail.

--- src/containers/TastingDetails/TastingDetails.js
+++ src/containers/TastingDetails/TastingDetails.js
@@ -1,9 +1,50 @@
-import { useParams } from "react-router";
+import { useEffect } from "react";
+import { useDispatch } from 'react-redux';
+import { useHistory, useParams } from "react-router";
+
+const CONFIRM_MESSAGE = 'Discard changes made?';
 
 const TastingDetails = () => {   
-    const {round} = useParams(); 
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const {id, round} = useParams(); 
+    const saved = false;
+    const saving = false;
+    const changes = false;
+    const loading = false;
+    const error = null;
+
     const roundIndex = round !== undefined ? (round - 1) : null;
 
-    return <span>Tasting Details - Round: {round}, Index:{roundIndex} </span>;
+    useEffect( () => {
+        if (saved === true) {
+            dispatch({id, roundIndex});
+            history.goBack();
+        }
+    }, [saved, history, dispatch, id, roundIndex]);
+    
+    const handleCancel = () => {
+        if (changes && !window.confirm(CONFIRM_MESSAGE) ) {
+            return;
+        }
+        history.goBack();
+    }
+
+    const handleDone = () => {
+        //Save the data.
+        history.goBack();
+    }
+
+    return <>
+        <button onClick={handleCancel}>{"Cancel"}</button>
+        Round {round}
+        <button onClick={handleDone}>{"Save"}</button>
+        <hr/>
+        {loading && <span>Loading...<br/></span>}
+        {saving && <span>Saving...<br/></span>}
+        {error && <span>{error.message}<br/></span>}
+        <br/>
+        <br/>
+    </>;
 }
 export default TastingDetails
