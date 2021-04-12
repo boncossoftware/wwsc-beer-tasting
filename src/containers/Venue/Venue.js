8d1e3aee9b4de701dc7497f34554bc33d5717068
Israel A. Posner <israelposner@boncos.io>
2021-04-12 11:07:49 -0400

    Added venue page.

--- src/containers/Venue/Venue.js
+++ src/containers/Venue/Venue.js
@@ -1,7 +1,35 @@
+import { format } from "date-fns";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router";
+
 const Venue = () => {    
-    
+    const {eventID} = useParams();
+    const { venue, date, price } = useSelector( s => s?.events?.items?.find( i => i.id === eventID ) ) || {};
+    const loading = useSelector( s => s?.events?.loading );
+
+    const renderTime = () => {
+        const formattedTime = date ? format(date, 'hh:mm aaa') : '-';
+        return <>Time: {formattedTime}<br/></>
+    }
+
+    const renderDate = () => {
+        const formattedDate = date ? format(date, 'dd MMM, yyyy') : 'No Date set';
+        return <>Date: {formattedDate}<br/></>
+    }
+
+    const renderPrice = () => {
+        return <>Price: {price}<br/> </>
+    }
+
     return <>
-        Venue
+        {loading && "loading..."}
+        {!loading && <>
+            <h2>{venue}</h2>
+            <br/>
+            {renderTime()}
+            {renderDate()}
+            {renderPrice()}
+        </>}
     </>
 }
 export default Venue;
