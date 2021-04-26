eeffe1634ba77f1e64400d4963b93f326478d0de
Israel A. Posner <israelposner@boncos.io>
2021-04-26 15:37:46 -0400

    Moved subscribe to the load function (temp fix for now).

--- src/store/reducers/events/listen-change.js
+++ src/store/reducers/events/listen-change.js
@@ -6,9 +6,17 @@ export const EVENT_MODIFIED_EVENT = 'event-modified';
 export const ACTION_EVENTS_LISTEN_CHANGE = 'events/listen_change';
 
 //Subscribe to event changes to listen for allow edit from owner.
-var subscribe;
-if (!subscribe) {
-    subscribe = firebase.firestore().collection('events').onSnapshot( (snapshot) => {
+var _unsubscribe;
+
+export function subscribe(userEmail) {
+    unsubscribe();
+    const relatedEventsCollectionRef = (
+        firebase.firestore()
+        .collection('events')
+        .where('related', 'array-contains', userEmail)
+    );
+
+    _unsubscribe = relatedEventsCollectionRef.onSnapshot( (snapshot) => {
         snapshot.docChanges().forEach( change => {
             if (change.type !== 'modified') return;
             
@@ -17,6 +25,11 @@ if (!subscribe) {
             window.dispatchEvent(windowEvent)
         });
     });
+}
+
+export function unsubscribe() {
+    (_unsubscribe && _unsubscribe());
+    _unsubscribe = undefined;
 }
 
 
