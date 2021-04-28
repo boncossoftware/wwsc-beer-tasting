5698765a096a0c17094d3797ea66833323f18b29
Israel A. Posner <israelposner@boncos.io>
2021-04-28 14:18:06 -0400

    Activated TS

--- src/components/authentication-form.tsx
+++ src/components/authentication-form.tsx
@@ -1,8 +1,28 @@
-const AuthenticationForm = ({id='authentication-form', submitButtonTitle='Login', onSubmit}) => {
-    const handleSubmit = (event) => {
+import { MouseEventHandler } from "react";
+
+type AuthenticationFormOnSubmitCallback = (
+    email:FormDataEntryValue|null, 
+    password: FormDataEntryValue|null
+) => void;
+
+interface AuthenticationFormProps {
+    id?: string,
+    submitButtonTitle?: string,
+    onSubmit?: AuthenticationFormOnSubmitCallback
+}
+const AuthenticationForm = (
+    {
+        id='authentication-form', 
+        submitButtonTitle='Login', 
+        onSubmit
+    }: AuthenticationFormProps
+) => {
+    const handleSubmit: MouseEventHandler = (event) => {
         event.preventDefault();
 
-        const form = document.getElementById(id);
+        const form = document.getElementById(id) as HTMLFormElement;
+        if (!form) return;
+
         const data = new FormData(form);
         const emailValue = data.get('email');
         const passwordValue = data.get('password');
