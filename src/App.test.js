8d3dd5b4d3d01542524e4e42b3687cfb86a03ad4
Israel A. Posner <israelposner@boncos.io>
2021-04-29 16:22:05 -0400

    Fixed test format.

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from './testing/test-utils';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const initialLoadingElement = screen.getByText('loading...');
-  expect(initialLoadingElement).toBeInTheDocument();
+    render(<App />);
+    const initialLoadingElement = screen.getByText('loading...');
+    expect(initialLoadingElement).toBeInTheDocument();
 });
