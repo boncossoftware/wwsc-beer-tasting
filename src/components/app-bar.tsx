c6bfd9eea0f76e7854394ab141d08044eb4dc03b
Israel A. Posner <israelposner@boncos.io>
2021-05-10 16:57:24 -0400

    Added tests (and fixed other tests).

---
+++ src/components/app-bar.tsx
@@ -0,0 +1,41 @@
+import { Toolbar } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+import { auth } from "store";
+import AccountButton from "./account-button";
+import { 
+    AppBar as BaseAppBar,
+    AppBarTitle
+} from "./app-bar.styles";
+
+export type AppBarProps ={
+    title: string|undefined;
+    renderLeftComponent?: () => JSX.Element | null | undefined;
+    renderRightComponent?: () => JSX.Element | null | undefined;
+}
+
+const AppBar = ({title, 
+    renderLeftComponent, 
+    renderRightComponent = () => null
+}:AppBarProps) => {
+    const dispatch = useDispatch();
+
+    const handleLogout = () => {
+        dispatch( auth.logout() );
+    }
+    
+    return (
+        <BaseAppBar>
+            <Toolbar>
+                {renderLeftComponent ? 
+                    renderLeftComponent() 
+                    : 
+                    <AccountButton onClickLogout={handleLogout} />
+                }
+                <AppBarTitle>{title}</AppBarTitle>
+                {renderRightComponent()}
+                
+            </Toolbar>
+        </BaseAppBar>
+    );
+}
+export default AppBar;
---
+++ src/containers/Events/Events.tsx
@@ -0,0 +1,75 @@
+import { MouseEventHandler, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router";
+import { TastingEvent } from "store/reducers/events/reducer";
+import { events, RootState, StoreError } from '../../store';
+import AppBar from 'components/app-bar';
+import {
+        AddButton, 
+        CircularProgress,
+        EventList, 
+        EventListItem, 
+        EventListItemDetails,
+        EventListSectionHeader
+} from './Events.styles';
+import ErrorMessage from "components/error-message";
+
+
+const Events = () => {
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const loading = useSelector<RootState, boolean>( s => s?.events?.loading );
+    const items = useSelector<RootState, TastingEvent[]|null>( s => s?.events?.items );
+    const error = useSelector<RootState, StoreError|null>( s => s?.events?.error );
+
+    useEffect( () => {
+        dispatch(events.load());
+    }, [dispatch] );
+
+    const handleAddEvent = () => {
+        history.push('/event/add');
+    }
+    
+    const handleClickEvent = (item: TastingEvent): MouseEventHandler => () => {
+        history.push(`/event/${item.id}`);
+    }
+
+    return <div id="events"> 
+        <AppBar 
+            title="Events" 
+            renderRightComponent={ () => <>
+                <AddButton onClick={handleAddEvent} disabled={loading}/>
+            </>}
+        />
+        <EventList>
+        {loading ? 
+            <CircularProgress />
+            :
+            <>
+                {error && <ErrorMessage>{`${error.message}(${error.code})`}</ErrorMessage>}
+                {items && items.map( (item, index) => {
+                    const newMonth = items[index-1]?.date?.getMonth() != item.date?.getMonth();
+                    return <>
+                        {newMonth && 
+                            <EventListSectionHeader 
+                                key={'header-'+index} 
+                            >
+                                {item.formattedMonth()}
+                            </EventListSectionHeader>
+                        }
+                        <EventListItem key={index} onClick={ handleClickEvent(item) }>
+                            <EventListItemDetails 
+                                name={item.name} 
+                                venue={item.venue} 
+                                date={item.formattedDate()}
+                            />
+                        </EventListItem>
+                    </>
+                })} 
+                {items?.length === 0 && <>No events.<br/></>}
+            </>
+        }
+        </EventList>
+    </div>;
+}
+export default Events;
