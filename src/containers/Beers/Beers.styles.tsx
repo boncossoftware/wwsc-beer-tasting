5c86559d48427a6ebf874e45204c1c1db165a55a
Israel A. Posner <israelposner@boncos.io>
2021-06-01 16:28:02 -0400

    Added padding at the bottom

--- src/containers/Beers/Beers.styles.tsx
+++ src/containers/Beers/Beers.styles.tsx
@@ -1,9 +1,14 @@
 import {
-    Container as BaseContainer
+    Container as BaseContainer, 
+    styled
 } from '@material-ui/core';
 import FullPageCircularProgress from "components/full-page-circular-progress";
 import BaseSection from 'components/section-grid';
 
 export const CircularProgress = FullPageCircularProgress;
 export const Section = BaseSection;
-export const Container = BaseContainer;
+export const Container = styled( ({...p}) =>
+    <BaseContainer {...p} />
+)(({theme}) => ({
+    paddingBottom: theme.spacing(4),
+}));
