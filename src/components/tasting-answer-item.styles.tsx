b110e6b366824a3e942dbba82406c267ca098d8c
Israel A. Posner <israelposner@boncos.io>
2021-06-02 14:45:35 -0400

    Added venue styling and tests.

--- src/components/tasting-answer-item.styles.tsx
+++ src/components/tasting-answer-item.styles.tsx
@@ -1,8 +1,17 @@
 import { 
-    styled, Typography, 
+    styled, 
+    ListSubheader as BaseListSubheader
 } from "@material-ui/core";
 import { ArrowForwardIos } from "@material-ui/icons";
 import BaseRating from "./rating"
+
+export const ListSubheader = styled(({...p}) => 
+    <BaseListSubheader disableGutters disableSticky {...p} />
+)(({theme}) => ({
+    color: theme.palette.primary.main,
+    backgroundColor: theme.palette.background.default,
+    height: theme.spacing(4)
+}))
 
 export const Asterisks = styled(({...p}) => 
     <span {...p}>✱</span>
