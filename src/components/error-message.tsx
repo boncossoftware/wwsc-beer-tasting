d5346515dd69dc1a0cf41ed8efddb4c733483dcf
Israel A. Posner <israelposner@boncos.io>
2022-10-19 16:16:23 -0400

    Fixed error message

--- src/components/error-message.tsx
+++ src/components/error-message.tsx
@@ -1,22 +1,21 @@
-import {
-    Alert
-} from '@material-ui/lab';
-import { 
-    styled, 
-} from '@material-ui/core';
+import { Alert } from "@material-ui/lab";
+import { styled } from "@material-ui/core";
 
-const ErrorMessage = styled( ({children, error, ...p})=>
-    <Alert 
-        elevation={6} 
-        variant="filled" 
-        severity="error" 
-        {...p}
-    >   
-        {error && `${error?.message}(${error?.code})` }
-        {children}
+const ErrorMessage = styled(({ children, error, ...p }) => {
+  const errorMessage = `${error?.message}(${error?.code})`;
+  return (
+    <Alert elevation={6} variant="filled" severity="error" {...p}>
+    {errorMessage}
+    {children}
     </Alert>
-)(({theme}) => ({
-    marginBottom: theme.spacing(3)
+  );
+})(({ theme }) => ({
+  marginBottom: theme.spacing(3),
+  width: "100%",
+  wordWrap: 'break-word',
+  '& .MuiAlert-message': {
+    wordBreak: 'break-word'
+  }
 }));
 
 export default ErrorMessage;
